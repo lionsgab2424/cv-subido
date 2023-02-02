2a3a68d170ab054354ce15889aa14b6c60159683
lionsgab2424 <124114558+lionsgab2424@users.noreply.github.com>
2023-02-02 01:12:02 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,36 @@
+function MoverDatPers() {
+        Element =document.getElementById("pers");
+        Element.src="Img/Foto.jpg";
+}
+
+function MoverDatPers2() {
+    Element =document.getElementById("pers");
+    Element.src="Img/Foto.jpg";
+}
+
+function infolaboral() {
+    Element =document.getElementById("laboral");
+    Element.src="Img/experiencia.jpg";
+}
+ function infolaboral2() {
+    Element =document.getElementById("laboral");
+    Element.src="Img/experiencia.jpg";
+}
+
+function habilidades() {
+    Element =document.getElementById("habilidad");
+    Element.src="Img/habilidad.jpg";
+}
+function habilidades2() {
+    Element =document.getElementById("habilidad");
+    Element.src="Img/habilidad.jpg";
+}
+
+function estudios() {
+    Element =document.getElementById("estudios");
+    Element.src="Img/estud.jpg";
+}
+function estudios2() {
+    Element =document.getElementById("estudios");
+    Element.src="Img/estud.jpg";
+}
